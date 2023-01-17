2b16d0d2e9451bcf4a4699778e720f3a25a6c12d
Jay M Gonzalez <jaimungon@gmail.com>
2023-01-17 02:11:08 +0100

    add configs

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,27 +1,29 @@
-const { ethers } = require("hardhat");
+const { ethers } = require('hardhat')
 
 async function main() {
-  const transactionCount = await owner.getTransactionCount();
+  const [deployer] = await ethers.getSigners()
+
+  const transactionCount = await deployer.getTransactionCount()
 
   // gets the address of the token before it is deployed
   const futureAddress = ethers.utils.getContractAddress({
-    from: owner.address,
-    nonce: transactionCount + 1
-  });
+    from: deployer.address,
+    nonce: transactionCount + 1,
+  })
 
-  const MyGovernor = await ethers.getContractFactory("MyGovernor");
-  const governor = await MyGovernor.deploy(futureAddress);
+  const MyGovernor = await ethers.getContractFactory('MyGovernor')
+  const governor = await MyGovernor.deploy(futureAddress)
 
-  const MyToken = await ethers.getContractFactory("MyToken");
-  const token = await MyToken.deploy(governor.address);
+  const MyToken = await ethers.getContractFactory('MyToken')
+  const token = await MyToken.deploy(governor.address)
 
   console.log(
     `Governor deployed to ${governor.address}`,
     `Token deployed to ${token.address}`
-  );
+  )
 }
 
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+  console.error(error)
+  process.exitCode = 1
+})
